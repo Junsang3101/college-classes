f7d6e9da8caefd3983c3ce93ff9f76d6e7af2505
Junsang3101 <3101junsang@gmail.com>
2025-03-27 16:43:20 +0900

    add 0327 lessons

---
+++ 기초 프로그래밍 실습/250327/score.py
@@ -0,0 +1,14 @@
+score = int(input("점수를 입력하세요 : "))
+
+if score >= 100:
+    print("A")
+elif score >= 90:
+    print("B")
+elif score >= 80:
+    print("C")
+elif score >= 70:
+    print("E")
+else:
+    print("F")
+
+print("학점입니다.")
